1850279f3796961d87cce1d7530d886496f39f34
Ben Chen <idben.chen@gmail.com>
2026-08-06 12:01:13 +0800

    待辦事項階段1，標題與統計區的視覺

---
+++ todolist01/src/Todo01.vue
@@ -0,0 +1,45 @@
+<script setup></script>
+
+<template>
+    <div class="container py-5">
+        <div class="card mw500px">
+            <div class="card-header bg-primary text-white text-center">
+                <!-- 標題與統計 -->
+                <h3 class="fw-bold">待辧事項清單</h3>
+
+                <div class="d-flex gap-3 justify-content-center">
+                    <div>
+                        <div class="fs-3 fw-bold">3</div>
+                        <div class="opacity-75">總計</div>
+                    </div>
+                    <div>
+                        <div class="fs-3 fw-bold">1</div>
+                        <div class="opacity-75">已完成</div>
+                    </div>
+                    <div>
+                        <div class="fs-3 fw-bold">2</div>
+                        <div class="opacity-75">待處理</div>
+                    </div>
+                </div>
+
+
+
+
+            </div>
+            <div class="card-body">
+                <!-- 輸入區 -->
+                <!-- 篩選按鈕 -->
+                <!-- 待辦事項區 -->
+            </div>
+            <div class="card-footer">
+                <!-- 其他功能 -->
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.mw500px {
+    max-width: 500px;
+}
+</style>
