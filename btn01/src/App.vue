c7f6001eb4607dcb6281cbbb59bebbd1cf89f680
Ben Chen <idben.chen@gmail.com>
2026-08-06 09:23:44 +0800

    組件專案初始化及組件 1

---
+++ btn01/src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import MyButton1 from './components/MyButton1.vue'
+</script>
+
+<template>
+  <div class="container py-3">
+    <MyButton1 /><br>
+    <MyButton1 /><br>
+    <MyButton1 />
+  </div>
+</template>
+
+<style scoped></style>
---
+++ btn01/src/components/MyButton1.vue
@@ -0,0 +1,7 @@
+<script setup></script>
+
+<template>
+    <div class="btn btn-danger mb-1">我是按鈕</div>
+</template>
+
+<style scoped></style>
