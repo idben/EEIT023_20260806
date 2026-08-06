9d162e2b4eecbd3c9445f018404b29c0bbee131f
Ben Chen <idben.chen@gmail.com>
2026-08-06 10:49:52 +0800

    scoped slot 向父層傳遞資料

---
+++ btn01/src/App2.vue
@@ -0,0 +1,37 @@
+<script setup>
+import MyButton2 from './components/MyButton2.vue'
+</script>
+
+<template>
+  <div class="container py-3">
+    <MyButton2 size="sm">
+      <template #default>
+        註冊
+      </template>
+      <template #icon="{ iconClass }">
+        <i :class="['fa-solid', 'fa-address-card', iconClass]"></i>
+      </template>
+    </MyButton2>
+
+    <br>
+
+    <MyButton2>
+      <template #icon="{ iconClass }">
+        <i :class="['fa-solid', 'fa-right-to-bracket', iconClass]"></i>
+      </template>
+      登入
+    </MyButton2>
+
+    <br>
+
+    <MyButton2 size="lg">
+      加入我的最愛
+      <template v-slot:icon="{ iconClass }">
+        <i :class="['fa-solid', 'fa-bookmark', iconClass]"></i>
+      </template>
+    </MyButton2>
+
+  </div>
+</template>
+
+<style scoped></style>
---
+++ btn01/src/components/MyButton2.vue
@@ -0,0 +1,40 @@
+<script setup>
+import { computed } from 'vue'
+
+const props = defineProps({
+  size: {
+    type: String,
+    default: "md"
+  }
+})
+
+const iconClass = computed(() => {
+  if (props.size == "sm") return 'fa-xs'
+  if (props.size == "lg") return 'fa-3x'
+  return 'fa-xl'
+})
+</script>
+
+<template>
+  <button :class="['btn', 'btn-danger', 'mb-1', 'btn-color1']">
+    <slot name="icon" :iconClass="iconClass"></slot>
+    <slot>預設文字</slot>
+  </button>
+</template>
+
+<style scoped>
+.btn-color1 {
+  --bs-btn-color: #fff;
+  --bs-btn-bg: #7c3aed;
+  --bs-btn-border-color: #7c3aed;
+  --bs-btn-hover-color: #fff;
+  --bs-btn-hover-bg: #510fba;
+  --bs-btn-hover-border-color: #6d28d9;
+  --bs-btn-active-color: #fff;
+  --bs-btn-active-bg: #5b21b6;
+  --bs-btn-active-border-color: #5b21b6;
+  --bs-btn-disabled-color: #fff;
+  --bs-btn-disabled-bg: #c4b5fd;
+  --bs-btn-disabled-border-color: #c4b5fd;
+}
+</style>
