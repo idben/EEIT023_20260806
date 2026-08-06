024c67d43aa91fe1f224f05f65de1a13513b06cf
Ben Chen <idben.chen@gmail.com>
2026-08-06 14:06:46 +0800

    待辦事項階段1 視覺完成

--- todolist01/src/Todo01.vue
+++ todolist01/src/Todo01.vue
@@ -28,11 +28,51 @@
             </div>
             <div class="card-body">
                 <!-- 輸入區 -->
+                <div class="input-group">
+                    <input type="text" class="form-control" placeholder="新增待辦事項...">
+                    <button class="btn btn-primary">新增</button>
+                </div>
                 <!-- 篩選按鈕 -->
+                <div class="d-flex gap-2 my-2">
+                    <button class="btn btn-light btn-primary  btn-sm rounded-pill">全部</button>
+                    <button class="btn btn-light btn-sm rounded-pill">待處理</button>
+                    <button class="btn btn-light btn-sm rounded-pill">已完成</button>
+                </div>
                 <!-- 待辦事項區 -->
+                <ul class="list-group">
+                    <li class="list-group-item d-flex gap-1 align-items-center">
+                        <span class="check-circle">
+                            <i class="fa-solid fa-check"></i>
+                        </span>
+                        <span class="completed-text">學習 Vue 3 基礎語法</span>
+                        <div class="btn btn-outline-danger btn-sm ms-auto">
+                            <i class="fa-solid fa-xmark"></i>
+                        </div>
+                    </li>
+                    <li class="list-group-item d-flex gap-1 align-items-center">
+                        <span class="check-circle">
+                            <i class="fa-solid fa-check"></i>
+                        </span>
+                        <span>學習 Vue 3 基礎語法</span>
+                        <div class="btn btn-outline-danger btn-sm ms-auto">
+                            <i class="fa-solid fa-xmark"></i>
+                        </div>
+                    </li>
+                    <li class="list-group-item d-flex gap-1 align-items-center">
+                        <span class="check-circle">
+                            <i class="fa-solid fa-check"></i>
+                        </span>
+                        <span>學習 Vue 3 基礎語法</span>
+                        <div class="btn btn-outline-danger btn-sm ms-auto">
+                            <i class="fa-solid fa-xmark"></i>
+                        </div>
+                    </li>
+                </ul>
             </div>
-            <div class="card-footer">
+            <div class="card-footer d-flex align-items-center">
                 <!-- 其他功能 -->
+                <span>2 項待處理</span>
+                <div class="btn btn-danger btn-sm ms-auto">清除已完成</div>
             </div>
         </div>
     </div>
@@ -41,5 +81,22 @@
 <style scoped>
 .mw500px {
     max-width: 500px;
+}
+
+.check-circle {
+    width: 24px;
+    height: 24px;
+    border: 2px solid #ddd;
+    border-radius: 50%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    cursor: pointer;
+    font-size: 12px;
+}
+
+.completed-text {
+    text-decoration: line-through;
+    color: #aaa;
 }
 </style>
